358b56ce9e0f60b0419d8e407057c69bb41f73a7
Vaibhav Kushwaha <vaibhav20092000@gmail.com>
2021-05-26 10:49:27 +0530

    action needed: figure out a way to workaround the AddNote() bug

--- lib/add_note.dart
+++ lib/add_note.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/note_class.dart';
 
 class AddNote extends StatefulWidget {
-  const AddNote({Key key}) : super(key: key);
+  const AddNote({Key key, this.note, this.isNew = false}) : super(key: key);
 
+  final Note note;
+  final bool isNew;
   @override
   _AddNoteState createState() => _AddNoteState();
 }
@@ -10,6 +13,19 @@ class AddNote extends StatefulWidget {
 class _AddNoteState extends State<AddNote> {
   String _title;
   String _content;
+  TextEditingController _titleController;
+  TextEditingController _noteContentController;
+  @override
+  void initState() {
+    super.initState();
+    if(!widget.isNew) {
+      _titleController = TextEditingController(
+          text: widget.note.title);
+      _noteContentController = TextEditingController(
+          text: widget.note.noteContent);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -19,6 +35,7 @@ class _AddNoteState extends State<AddNote> {
           children: [
             // for title
             TextField(
+              controller: _titleController,
               decoration: InputDecoration(
                 border: InputBorder.none,
                 hintText: 'Title',
@@ -27,8 +44,6 @@ class _AddNoteState extends State<AddNote> {
                   onPressed: () {
                     // do something to save the note
                     // add null checks too
-                    // todo: remove the following print statement
-                    print(_content);
                   },
                 )
               ),
@@ -38,6 +53,7 @@ class _AddNoteState extends State<AddNote> {
             ),
             // for note content
             TextField(
+              controller: _noteContentController,
               decoration: InputDecoration(
                 hintText: 'Enter your content',
                 border: InputBorder.none,
--- lib/my_notes_app.dart
+++ lib/my_notes_app.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app/note_class.dart';
+import 'package:notes_app/note_container.dart';
 
 class MyNotesApp extends StatefulWidget {
   const MyNotesApp({Key key}) : super(key: key);
@@ -8,6 +12,12 @@ class MyNotesApp extends StatefulWidget {
 }
 
 class _MyNotesAppState extends State<MyNotesApp> {
+
+  @override
+  void dispose() {
+    Hive.close();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +32,22 @@ class _MyNotesAppState extends State<MyNotesApp> {
           ),
           onPressed: () {
             Navigator.pushNamed(context, '/addNote');
+          },
+        ),
+        body: ValueListenableBuilder(
+          valueListenable: Hive.box<Note>('noteBox').listenable(),
+          builder: (context, Box<Note> _box, _) {
+            return ListView.builder(
+              itemCount: _box.values.length,
+              itemBuilder: (context, index) {
+                final _boxData = _box.getAt(index);
+                return NoteContainer(
+                  title: _boxData.title,
+                  noteContent: _boxData.noteContent,
+                  timeModified: _boxData.timeModified,
+                );
+              },
+            );
           },
         ),
       ),
