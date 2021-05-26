efbe2f6f0e1d6082029c2e4926f81369ce5b7c14
Vaibhav Kushwaha <vaibhav20092000@gmail.com>
2021-05-26 10:45:16 +0530

    initialized: hive path and TypeAdapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:notes_app/add_note.dart';
 import 'package:notes_app/my_notes_app.dart';
+import 'package:notes_app/note_class.dart';
+import 'package:path_provider/path_provider.dart' as pathProvider;
 
-void main()
+Future<void> main() async
 {
+  WidgetsFlutterBinding.ensureInitialized();
+  Directory directory = await pathProvider.getApplicationDocumentsDirectory();
+  Hive.init(directory.path);
+  Hive.registerAdapter(NoteAdapter());
+  await Hive.openBox<Note>('noteBox');
+
   runApp(MaterialApp(
     routes: {
       '/': (context) => MyNotesApp(),
